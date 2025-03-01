4cd32b8afbf6e472a6add4237d4a67ab652dee10
basakfulya <basakfulyaytu@gmail.com>
2025-03-02 00:49:43 +0300

    update

---
+++ 5_Jquery/js/a001_selectors.js
@@ -0,0 +1,15 @@
+$("button").click(function() {
+
+    $('p').hide(); //i gave a click property to button and such that i closed it.
+    // $('#id-jquery').hide();
+    // $('#id-jquery').hide(); 
+    // $('.class-jquery').hide(); //when you click the click button, this "class-jquery" will close immediately    
+    // $('*').hide(); //asterix will close all tags
+    // $("ul li").hide(); // this hide all the list given in code
+    // $("ul li:first").hide();
+    // $('ul li:first-child').hide();
+    // $("tr td").hide();
+    // S$("tr:even").hide();  
+    // $('tr:odd').hide();
+
+})
